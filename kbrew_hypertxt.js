83b76fec553e6712415032abf191987ee9cfe634
Kaden Griffith <kadengriffith@gmail.com>
2019-05-06 13:34:13 -0600

    Reduction to element manipulation. Added more FontAwesome features. Fixed whitespace issue with icon elements

--- kbrew_hypertxt.js
+++ kbrew_hypertxt.js
@@ -1,83 +1,46 @@
 // author   : Kaden Griffith
 // filename : kbrew_hypertxt.js
-// version  : 1.0.3
+// version  : 1.0.5
 
 'use strict';
 
 class kbrew_hypertxt {
   // Hypertxt constructor
-  constructor(title = '') {
-    this.title = title;
-    this.html = '';
+  constructor() {
+    this.version = "1.0.5";
   }
 
   /*
-   *  -- Prototypes
+   *  -- Prototype methods
    */
-
-  // Write HTML document
-  getFile(properties) {
-    this.clear();
-    this.writeOpenElement({
-      tag: '!DOCTYPE html'
-    });
-    this.write({
-      tag: 'html',
-      contains: this.getElement({
-        tag: 'head',
-        contains: this.getOpenElement({
-          tag: 'meta',
-          charset: 'UTF-8'
-        }) + this.getOpenElement({
-          tag: 'meta',
-          name: 'viewport',
-          content: 'width=device-width, initial-scale=1.0'
-        }) + this.getOpenElement({
-          tag: 'meta',
-          content: 'ie=edge'
-        }) + this.getElement({
-          tag: 'title',
-          contains: this.title
-        }) + this.processContent(properties.head)
-      }) + this.getElement({
-        tag: 'body',
-        contains: this.processContent(properties.body)
-      })
-    });
-    return this.html;
-  }
-
-  // Write element with configurable tag type and properties
-  write(properties) {
-    this.html += this.getElement(properties);
-  }
-
-  // Write an open ended element with configurable tag type and properties
-  writeOpenElement(properties) {
-    this.html += this.getOpenElement(properties);
-  }
 
   // Returns an element with configurable tag type and properties
   getElement(properties) {
     let str_element = properties.tag ? `<${properties.tag}` : '<div';
+
     for (let property in properties) {
       if (!/(tag|contains|icon)/.test(property)) str_element += ` ${property}="${properties[property]}"`;
     }
+
     str_element += `>${this.processContent(properties.contains)}`;
+
     str_element += properties.tag ? this.closeElement(properties) : this.closeElement({
       tag: 'div'
     });
+
     return str_element;
   }
-
 
   // Returns an open ended element with configurable tag type and properties
   getOpenElement(properties) {
     let str_element = `<${properties.tag}`;
+
     for (let property in properties) {
       if (!/(tag|contains|icon)/.test(property)) str_element += ` ${property}="${properties[property]}"`;
     }
+
     str_element += '>';
+
     return str_element;
   }
 
@@ -101,22 +64,36 @@ class kbrew_hypertxt {
   // Return a FontAwesome icon if Library is present in build
   icon(properties) {
     properties.tag = 'i';
-    properties.class = `fas fa-${properties.icon} ${this.processContent(properties.class)}`;
+    properties.class = this.removeLeadingWhitespace(`${this.processContent(properties.class)} fas fa-${properties.icon}`);
     return this.getElement(properties);
   }
 
   // Return a FontAwesome icon of type 'far'
   oIcon(properties) {
     properties.tag = 'i';
-    properties.class = `far fa-${properties.icon} ${this.processContent(properties.class)}`;
+    properties.class = this.removeLeadingWhitespace(`${this.processContent(properties.class)} far fa-${properties.icon}`);
     return this.getElement(properties);
   }
 
   // Return a FontAwesome icon of type 'fab'
   bIcon(properties) {
     properties.tag = 'i';
-    properties.class = `fab fa-${properties.icon} ${this.processContent(properties.class)}`;
+    properties.class = this.removeLeadingWhitespace(`${this.processContent(properties.class)} fab fa-${properties.icon}`);
+
     return this.getElement(properties);
+  }
+
+  // Return a FontAwesome icon of type 'fal'
+  // FontAwesome Premium must be active on the site
+  lIcon(properties) {
+    properties.tag = 'i';
+    properties.class = this.removeLeadingWhitespace(`${this.processContent(properties.class)} fal fa-${properties.icon}`);
+
+    return this.getElement(properties);
+  }
+
+  removeLeadingWhitespace(what) {
+    return what.replace(/^\s+/, '');
   }
 
   // Filter undefined innerHTML
@@ -127,21 +104,30 @@ class kbrew_hypertxt {
   // Get HTML document element
   get(e, index) {
     if (!e) return this.query('body');
+
     if (e.includes('#')) {
-      return document.getElementById(el.replace(/#/, ''));
+      return document.getElementById(e.replace(/#/, ''));
     } else if (e.includes('.')) {
-      return index ? document.getElementsByClassName(el.replace(/./, ''))[index] : document.getElementsByClassName(el.replace(/./, ''))[0];
+      return index ? document.getElementsByClassName(e.replace(/./, ''))[index] : document.getElementsByClassName(e.replace(/./, ''))[0];
     }
   }
 
   // Adds incoming string to a target HTML document element
   add(ref, what = '') {
-    ref.innerHTML += what;
+    if (typeof ref === "string") {
+      this.get(ref).innerHTML += what;
+    } else {
+      ref.innerHTML += what;
+    }
   }
 
-  // Removes
-  subtract(classOrID) {
-    get(classOrID) ? this.get(classOrID).remove() : console.error(`Class or Id ${classOrID} could not be selected.`);
+  // Removes an element
+  remove(classOrID, index) {
+    if (index) {
+      return this.get(classOrID, index) ? this.get(classOrID, index).remove() : false;
+    } else {
+      return this.get(classOrID) ? this.get(classOrID).remove() : false;
+    }
   }
 
   // Returns querySelector targeting the incoming string
@@ -151,7 +137,7 @@ class kbrew_hypertxt {
 
   // Returns querySelectorAll targeting the incoming string
   queryAll(target) {
-    return document.querySelectorAll(target.replace(/(#|.)/, ''));
+    return document.querySelectorAll(target);
   }
 
   // Find key value in JSON file
@@ -159,10 +145,20 @@ class kbrew_hypertxt {
     return JSON.parse(JSON.stringify(OBJ))[key];
   }
 
-  // Clears all internally written data
-  clear() {
-    this.html = '';
+  // Clears all content in an element
+  clear(e) {
+    if (e) {
+      if (typeof e === 'string') {
+        const element = this.get(e);
+
+        if (element) {
+          element.innerHTML = '';
+        }
+      } else {
+        e.innerHTML = '';
+      }
+    }
   }
 }
 
-module.exports = (title) => new kbrew_hypertxt(title);
+module.exports = new kbrew_hypertxt();
